ba14f6807640b09b4bfdd4e0fb96b5ec0eaafcb0
Davi Rodrigues <davirodrigues8@gmail.com>
2021-04-18 20:32:41 -0300

    add scroll, text info and button

--- lib/main.dart
+++ lib/main.dart
@@ -13,41 +13,62 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Calculadora de IMC'),
-        centerTitle: true,
-        backgroundColor: Colors.green,
-        actions: [
-          IconButton(icon: Icon(Icons.clear), onPressed: () {}),
-        ],
-      ),
-      backgroundColor: Colors.white,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Icon(
-            Icons.person_outline,
-            size: 120.0,
-            color: Colors.green,
+        appBar: AppBar(
+          title: Text('Calculadora de IMC'),
+          centerTitle: true,
+          backgroundColor: Colors.green,
+          actions: [
+            IconButton(icon: Icon(Icons.clear), onPressed: () {}),
+          ],
+        ),
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Icon(
+                Icons.person_outline,
+                size: 120.0,
+                color: Colors.green,
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: 'Peso (kg)',
+                    labelStyle: TextStyle(color: Colors.green)),
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.green, fontSize: 25.0),
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: 'Altura (cm)',
+                    labelStyle: TextStyle(color: Colors.green)),
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.green, fontSize: 25.0),
+              ),
+              Padding(
+                  padding: EdgeInsets.only(top: 10.0, bottom: 30.0),
+                  child: Container(
+                      height: 50.0,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        child: Text(
+                          'Calcular',
+                          style: TextStyle(color: Colors.white, fontSize: 25.0),
+                        ),
+                        style: ButtonStyle(
+                            backgroundColor:
+                                MaterialStateProperty.all(Colors.green)),
+                      ))),
+              Text(
+                'Info',
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.green, fontSize: 25.0),
+              )
+            ],
           ),
-          TextField(
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-                labelText: 'Peso (kg)',
-                labelStyle: TextStyle(color: Colors.green)),
-            textAlign: TextAlign.center,
-            style: TextStyle(color: Colors.green, fontSize: 25.0),
-          ),
-          TextField(
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-                labelText: 'Altura (cm)',
-                labelStyle: TextStyle(color: Colors.green)),
-            textAlign: TextAlign.center,
-            style: TextStyle(color: Colors.green, fontSize: 25.0),
-          ),
-        ],
-      ),
-    );
+        ));
   }
 }
